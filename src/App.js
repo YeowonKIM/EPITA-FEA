e6de2a5cf5810082f1e57511af513136d330944d
YeowonKIM <kkyoo152@gmail.com>
2025-06-25 01:05:19 +0200

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
+        <Route>
+          <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />}/>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;

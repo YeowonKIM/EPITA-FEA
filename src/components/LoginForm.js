1490654f7ab2fff19566c096280437d8173b01d6
YeowonKIM <kkyoo152@gmail.com>
2025-06-25 03:00:22 +0200

    Login page, form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
+
+export const LoginForm = () => {
+    const {email, setEmail} = useState("")
+    const {password, setPassword} = useState("")
+    const {showPass, setShowPass} = useState(false)
+
+    const {message, setMessage} = useState("")
+
+    const togglePassword = (event) => {
+        event.preventDefault();
+
+        setShowPass(!showPass)
+    }
+
+    const authentication = (event) => {
+        event.preventDefault();
+
+        if (!email || !password) {
+            setMessage("Please fill all required fields")
+        } else {
+            // make an http request to the backend
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <label className='email'>email</label>
+            <input
+            type='text'
+            className='email'
+            onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <label className='password'>Password</label>
+            <input
+            type={showPass ? "test" : "password"}
+            className='password'
+            onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <span onClick={(e) => togglePassword(e)} style={{cursor: "pointer"}}>
+                <span>
+                    {showPass ? (
+                        <FontAwesomeIcon icon={faEye} className='customIcon' />
+                    ) : (
+                        <FontAwesomeIcon icon={faEyeSlash} className='customIcon' />
+                    )
+                }
+                </span>
+            </span>
+            <button className='submit' onClick={(e) => authentication(e)}>
+                sumbit
+            </button>
+            <span style={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
+                {message}
+            </span>
+        </React.Fragment>
+    )
+}
+
+export default LoginForm
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import '../styles/LoginPage.css';
+import LoginForm from "../components/LoginForm";
+import { RegisterForm } from "../components/RegisterForm";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>
+    // Controls which form (Login, register) visible
+    const [registerFormStatus, setRegistrationFormStatus] = useState(false);
+
+    // Login form - sliding animation
+    const loginProps = useSpring({
+        left: registerFormStatus ? -500 : 0,
+    });
+
+    // Register form - sliding animation
+    const registerProps = useSpring({
+        left: registerFormStatus ? 0 : 500,
+    });
+
+    // Login button - border animation
+    const loginButtonProps = useSpring({
+        bordeBottom: registerFormStatus ? "solid 0px transparent" : "solid 2px white"
+    });
+
+    // Register button - border animation
+    const registerButtonProps = useSpring({
+        bordeBottom: registerFormStatus ? "solid 2px white" : "solid 0px transparent"
+    });
+
+    // Show login form when "Login" button is clicked
+    const loginClicked = () => {
+        setRegistrationFormStatus(false)
+    }
+    
+     // Show register form when "Register" button is clicked
+    const registerClicked = () => {
+        setRegistrationFormStatus(true)
+    }
+
+    return (<div className="container">
+        <div className="login-wrapper">
+            {/* toggle buttons */}
+            <div className="nav-buttons">
+                <animated.button id="loginButton" onClick={loginClicked} style={loginButtonProps}>
+                    Login
+                </animated.button>
+                <animated.button id="loginButton" onClick={registerClicked} style={registerButtonProps}>
+                    Register
+                </animated.button>
+            </div>
+
+            {/* Animated form container */}
+            <div className="form-group">
+                <animated.form action="" id="loginform" style={loginProps}> 
+                    <LoginForm />
+                </animated.form>
+                <animated.form action="" id="registerform" style={registerProps}>
+                    <RegisterForm />
+                </animated.form>
+            </div>
+        </div>
+    </div>
+    );
 };
 
 export default LoginPage;
